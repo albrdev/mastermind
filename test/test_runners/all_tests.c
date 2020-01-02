c895433d104dae5193d94ae8bdd486075fef819b
albrdev <albrdev@gmail.com>
2020-01-02 17:02:17 +0100

    -

--- test/test_runners/all_tests.c
+++ test/test_runners/all_tests.c
@@ -5,7 +5,6 @@ static void RunAllTests(void)
     RUN_TEST_GROUP(ProductionCode);
 }
 
-// Problem when running executable. May be this issue: https://github.com/ThrowTheSwitch/Unity/issues/402
 int main(int argc, const char * argv[])
 {
     //return UnityMain(argc, argv, RunAllTests);
